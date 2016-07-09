9c3a71bb81fd202beb19044fcda511eff566596e
Christopher Porter <snooper90@gmail.com>
2016-07-09 11:40:35 -0500

    add api_key and request.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
-
+var API_KEY = process.env.Wayblazer_API_KEY
+var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

7a3055790293c8a3151a318cd0a91956c2b34d08
Susannah Waite <susannahwaite2@gmail.com>
2016-07-09 17:07:10 -0500

    form added to landing page. mvp cssfor landing page?

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.btn-primary').click(function () {
     $('html,body').animate({
-        scrollTop: $(document).height()/5,
+        scrollTop: $(document).height()/2,
     }, 1000);
   });
 });
